21398b51ddeb161c29d3b18893069f697d67a896
parvathya150 <parvathyarun1111@gmail.com>
2025-08-11 21:37:33 +0530

    package

---
+++ Demo.java
@@ -0,0 +1,7 @@
+package demo;
+public class Demo {
+public String message = "Hello from the Demo class in another package!";
+public void displayMessage() {
+System.out.println("This method is called from outside the 'demo' package.");
+    }
+}
